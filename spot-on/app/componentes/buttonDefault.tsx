3b6c33bf0425d62fda07968c9d580033866dc06b
André Martins <fisio.andremartins88@gmail.com>
2026-02-19 11:34:21 +0000

    feat: add login components and update styles
    - Created a new `CaixaTextoLogin` component for email input.
    - Updated `ButtonDefault` component to use Next.js `Link` for navigation.
    - Modified `Home` page to include the new components and improved layout.
    - Added a new RTF file for project documentation.
    - Updated global styles for consistent padding.
    - Added `next-auth` dependency in `package.json`.
    - Updated package versions in `package-lock.json` and `package.json`.

--- spot-on/app/componentes/buttonDefault.tsx
+++ spot-on/app/componentes/buttonDefault.tsx
@@ -1,10 +1,18 @@
-function ButtonDefault(): JSX.Element {
-  const tituloButton: string = "Login";
+import Link from "next/link";
 
+type ButtonDefaultProps = {
+  href: string;
+  children: React.ReactNode; //quase dizer que pode assumir qlqr tipo/renderizar qlqr coisa
+};
+
+function ButtonDefault({ href, children }: ButtonDefaultProps) {
   return (
-    <button className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 hover:scale-105 transition duration-200">
-      {tituloButton}
-    </button>
+    <Link
+      href={href}
+      className="px-6 py-3 bg-blue-700 text-white font-semibold rounded-lg hover:bg-blue-800 hover:scale-105 transition duration-200"
+    >
+      {children}
+    </Link>
   );
 }
 
