db8bfe15e67c44026fe5b240d7943b4f7c500a97
Vasco Hilario <vascomouraohilario@gmail.com>
2026-02-26 13:08:43 +0000

    Main logic for creating sessions for a determined space

---
+++ spot-on/app/api/spaces/[spaceId]/sessions/route.ts
@@ -0,0 +1,58 @@
+import { prisma } from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+// We need to implement logic to delete the session when time expires
+
+export async function POST(
+    request: Request,
+    { params }: { params: { spaceId: string } }
+) {
+    try {
+        const { hostId } = await request.json();
+        
+        // Check if space exists
+        const space = await prisma.space.findUnique({
+            where: { id: params.spaceId }
+        });
+        
+        if (!space) {
+            return NextResponse.json(
+                { error: 'Space not found' },
+                { status: 404 }
+            );
+        }
+        
+        const existingSession = await prisma.studySession.findFirst({
+            where: {
+                spaceId: params.spaceId,
+                status: 'ACTIVE'
+            }
+        });
+        
+        if (existingSession) {
+            return NextResponse.json(
+                { error: 'Space is already occupied' },
+                { status: 409 } // 409 Conflict
+            );
+        }
+        
+        // Create the new session
+        const newSession = await prisma.studySession.create({
+            data: {
+                spaceId: params.spaceId,
+                hostId,
+                expectedEndTime: new Date(Date.now() + 60 * 60 * 1000) // Set expected end time to 1 hour from now, but can be adjusted as needed
+            },
+            include: { space: true, host: true }
+        });
+        
+        return NextResponse.json(newSession, { status: 201 });
+    } catch (error) {
+        console.error('Error creating session:', error);
+        return NextResponse.json(
+            { error: 'Failed to create session' },
+            { status: 500 }
+        );
+    }
+}
+
